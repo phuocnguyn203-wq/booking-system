a1ebb6079750cfcd151332ff3ec07ca917639f1f
phuocnguyn203-wq <phuocnguyn203@gmail.com>
2026-08-18 00:43:56 +0700

    Implment RoomRepository.createRoom

--- src/app/repositories/room.repository.js
+++ src/app/repositories/room.repository.js
@@ -1,8 +1,11 @@
+import { Errors } from '../errors/errorDefinitions.js';
+import { createAppError } from '../errors/AppError.js';
+
 function mapRowToRoom(row) {
   return {
     id: row.id,
     name: row.name,
-    price_per_night: row.price_per_night,
+    pricePerNight: Number(row.price_per_night),
     created_at: row.created_at,
   };
 }
@@ -26,5 +29,24 @@ export class RoomRepository {
     return mapRowToRoom(result.rows[0])
   }
 
-  async createRoom({})
+  async createRoom({name, pricePerNight}) {
+    try { 
+      const result = await this.query(
+        `
+        INSERT INTO rooms (name, price_per_night)
+        VALUES
+        ($1, $2)
+        RETURNING *
+        `,
+        [name, pricePerNight]
+      )
+
+      return mapRowToRoom(result.rows[0])
+    } catch (error) {
+      if (error.code === '23505')
+        throw createAppError(Errors.ROOM_ALREADY_EXISTS)
+      // TODO: It must throw DataAccessError here because it's unknown
+      throw error
+    }
+  }
 }
