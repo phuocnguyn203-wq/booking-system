d826fba20b065e9693a5ab4bf0be6c1d60a97d5f
phuocnguyn203-wq <phuocnguyn203@gmail.com>
2026-08-18 23:09:39 +0700

    Add new error definitions for validation errors

--- src/app/errors/errorDefinitions.js
+++ src/app/errors/errorDefinitions.js
@@ -8,5 +8,20 @@ export const Errors = {
     statusCode: 500,
     code: 'INTERNAL ERROR',
     message: 'Internal error'
+  },
+  NOT_NULL_VALIDATION: {
+    statusCode: 400,
+    code: 'NOT_NULL_VALIDATION',
+    message: 'Required field is missing'
+  },
+  UNIQUE_VALIDATION: {
+    statusCode: 409,
+    code: 'UNIQUE_VALIDATION',
+    message: 'An account with provided information already exists.'
+  },
+  EMAIL_VALIDATION: {
+    statusCode: 409,
+    code: 'EMAIL_VALIDATION',
+    message: 'Email already exists'
   }
 }
