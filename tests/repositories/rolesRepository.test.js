a79813562e6abcbce26cee12e5b2196b233f1202
phuocnguyn203-wq <phuocnguyn203@gmail.com>
2026-08-23 21:53:29 +0700

    Add tests for createRole method in RoleRepository

--- tests/repositories/rolesRepository.test.js
+++ tests/repositories/rolesRepository.test.js
@@ -41,3 +41,74 @@ describe('RoleRepository [findById]', () => {
   })
 
 })
+
+describe('RoleRepository [createRole]', () => {
+  it('returns role object and inserts into roles table', async ({ roleRepository }) => {
+    // Arrange
+    const roleInfo = {
+      code: 'super_important_code',
+      name: 'Dark Accountant',
+      description: 'Do dark account to vanish tax'
+    }
+    
+    // Act
+    const role = await roleRepository.createRole(roleInfo)
+
+    // Assert
+    expect(role).toMatchObject(roleInfo)
+    // Assert side effect
+    const rowResult = await query(
+      `SELECT id, code, name, description, is_active FROM roles WHERE id=$1`,
+      [role.id]
+    )
+    const row = rowResult.rows[0]
+    expect(row).toMatchObject(roleInfo)
+  })
+
+  it('throws AppError when given duplicated code', async ({ roleRepository}) => {
+    // Arrange
+    const testRole = await createTestRole()
+    const duplicatedCodeInfo = {
+      code: testRole.role,
+      name: 'Mysterious role',
+      description: null,
+    }
+    
+    // Act
+    const rolePromise = roleRepository.createRole(duplicatedCodeInfo)
+
+    // Assert
+    await expect(rolePromise).rejects.toMatchObject({
+      statusCode: 409,
+      message: 'A role with this information already exists'
+    })
+  })
+
+  it.each([
+    [
+      'code', 
+      {
+        name: 'Administrator',
+        description: 'Manages the system'
+      }
+    ],
+    [
+      'name',
+      {
+        code: 'admin',
+        description: 'Manages the system'
+      }
+    ]
+  ])('throw AppError when not given %s', async (_, incompleteInfo, { roleRepository }) => {
+    // Arrange
+
+    // Act
+    const rolePromise = roleRepository.create(incompleteInfo)
+
+    // Assert
+    await expect(rolePromise).rejects.toMatchObject({
+      statusCode: 400,
+      message: 'Required fields are mising'
+    })
+  })
+})
